732a7d55e68c8c217bee48d52a26ba1ea2870dd8
aygaoua <102914808+aygaoua@users.noreply.github.com>
2024-05-25 17:38:07 +0100

    cmnt the no needed part (topic class) and add the main tools for doing the topic command

--- mandatory/topic.cpp
+++ mandatory/topic.cpp
@@ -6,42 +6,98 @@
 /*   By: azgaoua <azgaoua@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 18:03:45 by azgaoua           #+#    #+#             */
-/*   Updated: 2024/05/22 06:05:10 by azgaoua          ###   ########.fr       */
+/*   Updated: 2024/05/24 02:57:07 by azgaoua          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/topic.hpp"
+#include "../include/server.hpp"
+#include "../include/reply.hpp"
 
 
-TOPIC::TOPIC() {
-    _topic_name = "default";
+void    Server::Topic_Command(std::vector<std::string> Topic, Client *user) {
+    std::string full_name_topic;
+
+    if (Topic[1][0] == '#') {
+        Topic[1].erase(0, 1);
+        for (size_t i = 0; i < channels.size(); i++) {
+            if (channels[i].getChannelName() == Topic[1]) {
+                if (Topic[2][0] == ':')
+                {
+                    Topic[2].erase(0, 1);
+                    for (size_t j = 2; j < Topic.size(); j++)
+                        full_name_topic += Topic[j];
+                    if (full_name_topic.empty())
+                    {
+                        sendToClient(user->get_fd(), RPL_TOPIC(user->get_hostname(), Topic[1], ""));
+                        channels[i].set_topic("");
+                    }
+                    else
+                    {
+                        sendToClient(user->get_fd(), RPL_TOPIC(user->get_hostname(), Topic[1], full_name_topic));
+                        channels[i].set_topic(full_name_topic);
+                    }
+                }
+                else
+                {
+                    sendToClient(user->get_fd(), RPL_NOTOPIC(user->get_hostname(), Topic[1]));
+                    std::cout << "Error: No topic (syntax `:`)" << std::endl;
+                }
+                return;
+            }
+        }
+    }
+    else {
+        std::cout << "Error: No such channel (syntax `#`)"<< std::endl;
+        // sendToClient(get_connect_client(atoi(Topic[0].c_str()))->get_fd(), REPLY_NOTONCHANNEL(Topic[1]));
+    }
 }
 
-TOPIC::TOPIC(const TOPIC &src) {
-    *this = src;
+std::string Channels::get_topic() {
+    return (topic);
 }
 
-TOPIC &TOPIC::operator=(const TOPIC &src) {
-    _topic_name = src._topic_name;
-    return *this;
+void Server::DisplayTopic(std::string channel_name, Client *user) {
+    for (size_t i = 0; i < channels.size(); i++) {
+        if (channels[i].getChannelName() == channel_name) {
+            if (channels[i].get_topic().empty())
+                sendToClient(user->get_fd(), RPL_NOTOPIC(user->get_hostname(), channel_name));
+            else
+                sendToClient(user->get_fd(), RPL_TOPIC(user->get_hostname(), channel_name, channels[i].get_topic()));
+        }
+    }
+    sendToClient(user->get_fd(), ERR_NOTONCHANNEL(user->get_hostname(), channel_name, user->get_nickname()));
 }
 
-void TOPIC::addTopic(std::string topic) {
-    _topic_name = topic;
-}
+// TOPIC::TOPIC() {
+//     _topic_name = "default";
+// }
 
-void TOPIC::removeTopic(std::string topic) {
-    _topic_name = topic;
-}
+// TOPIC::TOPIC(const TOPIC &src) {
+//     *this = src;
+// }
 
-void TOPIC::displayTopic() {
-    std::cout << "Topic: " << _topic_name << std::endl;
-}
+// TOPIC &TOPIC::operator=(const TOPIC &src) {
+//     _topic_name = src._topic_name;
+//     return *this;
+// }
 
-void TOPIC::displayMessage(std::string topic, std::string message) {
-    std::cout << "Topic: " << topic << std::endl;
-    std::cout << "Message: " << message << std::endl;
-}
+// void TOPIC::addTopic(std::string topic) {
+//     _topic_name = topic;
+// }
 
-TOPIC::~TOPIC() {
-}
+// void TOPIC::removeTopic(std::string topic) {
+//     _topic_name = topic;
+// }
+
+// void TOPIC::displayTopic() {
+//     std::cout << "Topic: " << _topic_name << std::endl;
+// }
+
+// void TOPIC::displayMessage(std::string topic, std::string message) {
+//     std::cout << "Topic: " << topic << std::endl;
+//     std::cout << "Message: " << message << std::endl;
+// }
+
+// TOPIC::~TOPIC() {
+// }
