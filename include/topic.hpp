96a7bffe742796b3a3a3bc41433eba79f496c5bf
aygaoua <102914808+aygaoua@users.noreply.github.com>
2024-05-25 17:33:58 +0100

    commant the topic class because no need to do it

--- include/topic.hpp
+++ include/topic.hpp
@@ -6,34 +6,31 @@
 /*   By: azgaoua <azgaoua@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 18:08:10 by azgaoua           #+#    #+#             */
-/*   Updated: 2024/05/22 06:04:50 by azgaoua          ###   ########.fr       */
+/*   Updated: 2024/05/24 02:27:09 by azgaoua          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOPIC_HPP
 # define TOPIC_HPP
 
-# include <iostream>
-# include <string>
-# include <vector>
-# include <map>
-# include <algorithm>
+
+# include "../include/server.hpp"
 
 
-class TOPIC {
-    private:
-        std::string     _topic_name;
+// class TOPIC {
+//     private:
+//         std::string     _topic_name;
 
-    public:
-        TOPIC           ();
-        ~TOPIC          ();
-        TOPIC           (const TOPIC &src);
-        TOPIC           &operator=(const TOPIC &src);
-        void            addTopic(std::string topic);
-        void            removeTopic(std::string topic);
-        void            displayTopic();
-        void            displayMessage(std::string topic, std::string message);
-};
+//     public:
+//         TOPIC           ();
+//         ~TOPIC          ();
+//         TOPIC           (const TOPIC &src);
+//         TOPIC           &operator=(const TOPIC &src);
+//         void            addTopic(std::string topic);
+//         void            removeTopic(std::string topic);
+//         void            displayTopic();
+//         void            displayMessage(std::string topic, std::string message);
+// };
 
 
 #endif
