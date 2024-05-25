56369b6e60d5c2bb67edee24c29d81b4300cb614
aygaoua <102914808+aygaoua@users.noreply.github.com>
2024-05-25 17:29:18 +0100

    add the topic to channels

--- include/channels.hpp
+++ include/channels.hpp
@@ -8,6 +8,7 @@
 #include <sys/socket.h>
 #include"client.hpp"
 #include "reply.hpp"
+#include "topic.hpp"
 
 
 class Channels
@@ -15,16 +16,16 @@ class Channels
     private:
         std::string         name;
         std::vector<int>    ClientssHouse;
-        // std::string         topic;
-        // bool                bool_topic;
+        std::string         topic;
+        bool                bool_topic;
         // std::vector<Client> ClientssHouse;
     public:
         Channels(std::string name);
         Channels();
         Channels(const Channels& copy);
         void join(int clientFd, Client *client);
-        // void set_topic(std::string topic);
-        // void get_topic(int clientFd);
+        void set_topic(std::string topic);
+        std::string get_topic();
         // bool clientExists(int clientFd);
         ~Channels();
         std::string getChannelName();
