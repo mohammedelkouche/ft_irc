bf8fc063ca8fd6cad98cda8d78b58d35dc5a6c36
aygaoua <zgaoua72@gmail.com>
2024-08-14 16:20:44 +0100

    fix the err_nosuchnick problem

--- mandatory/PrivateMessage.cpp
+++ mandatory/PrivateMessage.cpp
@@ -17,76 +17,92 @@ void Server::sendToChannel(Client *user, const std::string& message, std::string
     }
 }
 
+std::vector<std::string> split(std::string commande, char delimeter)
+{
+    std::stringstream ss(commande);
+    std::string item;
+    std::vector<std::string> splittedStrings;
+    while (std::getline(ss, item, delimeter))
+       splittedStrings.push_back(item);
+    return splittedStrings;
+}
+
 void Server::Private_message(std::vector<std::string> commande, Client *user)
 {
+    std::vector<std::string> targets;
     std::string full_message;
     size_t i;
 
-    if (commande.size() == 1)
+    targets =  split(commande[1], ',');
+
+    if (commande.size() <= 2)
     {
         sendToClient(user->get_fd(), ERROR_NEEDMOREPARAMS(user->get_nickname(), user->get_hostname()));
         return ;
     }
-    if (commande[1][0] == '#')
+    for(std::vector<std::string>::iterator it_t = targets.begin(); it_t != targets.end(); ++it_t)
     {
-        if (no_suck_channel(commande) == 1)
+        if ((*it_t)[0] == '#')
         {
-            sendToClient(user->get_fd(), ERROR_NOSUCHCHANNEL(user->get_hostname(), commande[1], user->get_nickname()));
-            return ;
-        }
-        for (std::vector<Channel*>::iterator it = channels.begin(); it != channels.end(); ++it)
-        {
-            if ((*it)->getChannelName() == commande[1])
+            if (no_suck_channel(commande) == 1)
             {
-                if (on_channel((*it)->GetClientssHouse(), user))
+                sendToClient(user->get_fd(), ERROR_NOSUCHCHANNEL(user->get_hostname(), (*it_t), user->get_nickname()));
+                continue ;
+            }
+            for (std::vector<Channel*>::iterator it = channels.begin(); it != channels.end(); ++it)
+            {
+                if ((*it)->getChannelName() == (*it_t))
                 {
-                    if (commande.size() >= 3 && commande[2][0] == ':')
+                    if (on_channel((*it)->GetClientssHouse(), user))
+                    {
+                        if (commande[2][0] == ':')
+                        {
+                            commande[2].erase(0, 1);
+                            for (size_t j = 2; j < commande.size(); j++)
+                                full_message += commande[j];
+                        }
+                        else
+                            full_message += commande[2];
+                        if (full_message.empty())
+                            sendToChannel(user, ERR_NOTEXTTOSEND(user->get_hostname()), (*it)->getChannelName());
+                        else
+                            sendToChannel(user, RPL_AWAY((*it)->getChannelName(), user->get_hostname(), full_message), (*it)->getChannelName());
+                        continue ;
+                    }
+                    else
+                        sendToClient(user->get_fd(), ERR_NOTONCHANNEL(user->get_hostname(), (*it)->getChannelName(), user->get_nickname()));
+                }
+            }
+        }
+        else
+        {
+            for (i = 0; i < clients.size(); i++)
+            {
+                if (clients[i].get_nickname() == (*it_t))
+                {
+                    if (commande[2][0] == ':')
                     {
                         commande[2].erase(0, 1);
                         for (size_t j = 2; j < commande.size(); j++)
                             full_message += commande[j];
+                        if (full_message.empty())
+                            sendToClient(user->get_fd(), ERR_NOTEXTTOSEND(user->get_hostname()));
+                        else
+                            sendToClient(clients[i].get_fd(), RPL_AWAY(clients[i].get_nickname(), user->get_hostname(), full_message));
                     }
-                    else if (commande.size() >= 3)
+                    else
+                    {
                         full_message += commande[2];
-                    if (full_message.empty())
-                        sendToChannel(user, ERR_NOTEXTTOSEND(user->get_hostname()), (*it)->getChannelName());
-                    else
-                        sendToChannel(user, RPL_AWAY(commande[1], user->get_hostname(), full_message), (*it)->getChannelName());
-                    return ;
+                        if (full_message.empty())
+                            sendToClient(user->get_fd(), ERR_NOTEXTTOSEND(user->get_hostname()));
+                        else
+                            sendToClient(clients[i].get_fd(), RPL_AWAY(clients[i].get_nickname(), user->get_hostname(), full_message));
+                    }
+                    break ;
                 }
-                else
-                    sendToClient(user->get_fd(), ERR_NOTONCHANNEL(user->get_hostname(), commande[1], user->get_nickname()));
             }
+            if (i >= clients.size())
+                sendToClient(user->get_fd(), ERR_NOSUCHNICK(user->get_hostname(), (*it_t)));
         }
-    }
-    else
-    {
-        for (i = 0; i < clients.size(); i++)
-        {
-            if (clients[i].get_nickname() == commande[1])
-            {
-                if (commande[2][0] == ':')
-                {
-                    commande[2].erase(0, 1);
-                    for (size_t j = 2; j < commande.size(); j++)
-                        full_message += commande[j];
-                    if (full_message.empty())
-                        sendToClient(user->get_fd(), ERR_NOTEXTTOSEND(user->get_hostname()));
-                    else
-                        sendToClient(clients[i].get_fd(), RPL_AWAY(commande[1], user->get_hostname(), full_message));
-                }
-                else
-                {
-                    full_message += commande[2];
-                    if (full_message.empty())
-                        sendToClient(user->get_fd(), ERR_NOTEXTTOSEND(user->get_hostname()));
-                    else
-                        sendToClient(clients[i].get_fd(), RPL_AWAY(commande[1], user->get_hostname(), full_message));
-                }
-                return ;
-            }
-        }
-        if (i >= clients.size())
-            sendToClient(user->get_fd(), ERR_NOSUCHNICK(user->get_hostname(), user->get_nickname()));
     }
 }
